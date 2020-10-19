c8bf9abba76df0f1eae4eaa3daeaef9f90a68629
Tendi F <tendifirmansyah30@gmail.com>
2020-10-19 22:41:12 +0700

    Initial Commit

---
+++ Search/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Search
+//
+//  Created by Macbook Pro on 10/19/20.
+//  Copyright © 2020 tendi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
